49ea1605eeabb2ee35ad65dcd47b8af1ad716acd
Andrew Kane <acekane1@gmail.com>
2016-03-27 14:49:22 -0700

    Started test branch

--- test/pgsync_test.rb
+++ test/pgsync_test.rb
@@ -2,6 +2,16 @@ require_relative "test_helper"
 
 class PgSyncTest < Minitest::Test
   def test_no_config
-    assert_raises(PgSync::Error, "Config not found") { PgSync::Client.new([]) }
+    assert_raises(PgSync::Error, "Config not found") { perform("", config: false) }
+  end
+
+  def test_config
+    assert perform("")
+  end
+
+  def perform(command, config: true)
+    command << " --config test/support/config.yml" if config
+    client = PgSync::Client.new(command.split(" "))
+    client.perform
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,9 @@
+$VERBOSE=false
+
 require "bundler/setup"
 Bundler.require(:default)
 require "minitest/autorun"
 require "minitest/pride"
+
+%x[psql -d pgsync_from -f test/support/from.sql]
+%x[psql -d pgsync_to -f test/support/to.sql]
