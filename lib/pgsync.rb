6f091064f318d59b026ee490a5dd2956814af928
Andrew Kane <acekane1@gmail.com>
2017-08-30 02:49:31 -0700

    Fixed --schema-only with non-lowercase tables - fixes #15

--- lib/pgsync.rb
+++ lib/pgsync.rb
@@ -9,6 +9,7 @@ require "multiprocessing"
 require "fileutils"
 require "tempfile"
 require "cgi"
+require "shellwords"
 
 module URI
   class POSTGRESQL < Generic
@@ -97,8 +98,8 @@ module PgSync
 
         if opts[:schema_only]
           log "* Dumping schema"
-          tables = tables.keys.map { |t| "-t #{t}" }.join(" ")
-          psql_version = Gem::Version.new(`psql --version`.lines[0].chomp.split(" ")[-1])
+          tables = tables.keys.map { |t| "-t #{Shellwords.escape(quote_ident(t))}" }.join(" ")
+          psql_version = Gem::Version.new(`psql --version`.lines[0].chomp.split(" ")[-1].sub(/beta\d/, ""))
           if_exists = psql_version >= Gem::Version.new("9.4.0")
           dump_command = "pg_dump -Fc --verbose --schema-only --no-owner --no-acl #{tables} #{to_url(source_uri)}"
           restore_command = "pg_restore --verbose --no-owner --no-acl --clean #{if_exists ? "--if-exists" : nil} -d #{to_url(destination_uri)}"
