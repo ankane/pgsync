90b3c0f2960abc934772304b34ab44472f802dce
Andrew Kane <andrew@chartkick.com>
2020-04-03 17:48:11 -0700

    Test help and version

--- test/pgsync_test.rb
+++ test/pgsync_test.rb
@@ -88,6 +88,12 @@ class PgSyncTest < Minitest::Test
 
   def test_version
     assert_prints PgSync::VERSION, "-v"
+    assert_prints PgSync::VERSION, "--version"
+  end
+
+  def test_help
+    assert_prints "Usage:", "-h"
+    assert_prints "Usage:", "--help"
   end
 
   def test_schema_only
