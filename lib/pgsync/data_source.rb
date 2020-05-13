21f2214c6b25626268fa59c0647c2f4d5de04ba5
Andrew Kane <andrew@chartkick.com>
2020-05-13 06:21:02 -0700

    Slightly better code

--- lib/pgsync/data_source.rb
+++ lib/pgsync/data_source.rb
@@ -179,11 +179,10 @@ module PgSync
 
     def conninfo
       @conninfo ||= begin
-        if conn.respond_to?(:conninfo_hash)
-          conn.conninfo_hash
-        else
+        unless conn.respond_to?(:conninfo_hash)
           raise Error, "libpq is too old. Upgrade it and run `gem install pg`"
         end
+        conn.conninfo_hash
       end
     end
 
