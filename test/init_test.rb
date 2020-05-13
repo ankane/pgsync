6b2afdb2b15aba0b03bab849b56dd3873f197994
Andrew Kane <andrew@chartkick.com>
2020-05-12 23:29:32 -0700

    Added init tests

--- test/init_test.rb
+++ test/init_test.rb
@@ -2,10 +2,33 @@ require_relative "test_helper"
 
 class InitTest < Minitest::Test
   def test_works
-    Dir.chdir(Dir.mktmpdir) do
+    new_dir do
       assert_works "--init"
-      assert File.exist?(".pgsync.yml")
-      assert_match /sslmode=require/, File.read(".pgsync.yml")
+      assert_match "?sslmode=require", File.read(".pgsync.yml")
+    end
+  end
+
+  def test_rails
+    new_dir do
+      Dir.mkdir("bin")
+      File.write("bin/rails", "")
+      assert_works "--init"
+      assert_match "schema_migrations", File.read(".pgsync.yml")
+    end
+  end
+
+  def test_heroku
+    new_dir do
+      system "git init --quiet"
+      system "git remote add heroku https://git.heroku.com/test.git"
+      assert_works "--init"
+      assert_match "$(heroku config:get DATABASE_URL)?sslmode=require", File.read(".pgsync.yml")
+    end
+  end
+
+  def new_dir
+    Dir.chdir(Dir.mktmpdir) do
+      yield
     end
   end
 end
