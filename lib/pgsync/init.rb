fa30e01df46af9474d159b809521b7187df6b703
Andrew Kane <andrew@chartkick.com>
2020-06-04 20:08:40 -0700

    Improved init options

--- lib/pgsync/init.rb
+++ lib/pgsync/init.rb
@@ -9,7 +9,20 @@ module PgSync
     end
 
     def perform
-      file = db_config_file(@arguments[0]) || config_file || ".pgsync.yml"
+      if @arguments.size > 1
+        raise Error, "Usage:\n    pgsync --init [db]"
+      end
+
+      file =
+        if @options[:config]
+          @options[:config]
+        elsif @arguments.any?
+          db_config_file(@arguments.first)
+        elsif @options[:db]
+          db_config_file(@options[:db])
+        else
+          ".pgsync.yml"
+        end
 
       if File.exist?(file)
         raise Error, "#{file} exists."
