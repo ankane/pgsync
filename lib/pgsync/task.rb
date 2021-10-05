bd64d5f30f94de75afb3e2e77ce8f35796ff7ab6
Marco Montagna <marco.montagna@instacart.com>
2021-10-05 13:26:48 -0700

    Fix

--- lib/pgsync/task.rb
+++ lib/pgsync/task.rb
@@ -187,7 +187,7 @@ module PgSync
           # creating many temp tables churns pg_attribute and pg_type
           # adding bloat and slowing down the DB
           if opts[:no_temp_table]
-            temp_table = "pgsync_#{table}"
+            temp_table = "pgsync_#{table.gsub('.', '_')}"
             if !table_schema_match(temp_table, table.name)
               log "Table schema does not match, dropping the staging table."
               destination.execute("DROP TABLE IF EXISTS #{quote_ident_full(temp_table)}")
